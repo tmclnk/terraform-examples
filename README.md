6fcddba0acfd7f262c843c4aaa37a514a60d8944
Tom McLaughlin <tom.mclaughlin@devobsessed.com>
2022-07-24 18:32:49 -0500

    Add elasticache resource

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
+# Terraform Basics
 
+- Hashicorp Configuration Language [HCL](https://github.com/hashicorp/hcl)
+    - Not YAML
+    - Declarative
+    - Typed - strings, numbers, lists, maps
+- "Providers" for different platforms
+    - Providers provide "resources",
+      e.g. [aws provider](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/)
+- Modules
+    - Group resources together
+    - Modules have [Input Variables](./modules/agility-stack/variables.tf)
+    - Modules have outputs,
+      e.g. [EKS Attributes](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_cluster#attributes-reference)
+- Data Lookups
+    - look up latest machine image for an OS
+    - look up things you created by hand, e.g. domains you registered
 
 # My Setup
 
--- modules/agility-stack/main.tf
+++ modules/agility-stack/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
   default_tags {
     tags = {
       Environment = var.environment
-      Owner = var.owner
+      Owner       = var.owner
       Application = "Agility"
     }
   }
@@ -16,29 +16,49 @@ provider "aws" {
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.2"
-  name = "${var.environment}-vpc"
+  name    = "${var.environment}-vpc"
 
   cidr = "10.0.0.0/16"
 
-  azs             = var.aws_azs
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-  database_subnets = ["10.0.103.0/24", "10.0.104.0/24"]
+  azs                 = var.aws_azs
+  private_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets      = ["10.0.101.0/24", "10.0.102.0/24"]
+  database_subnets    = ["10.0.103.0/24", "10.0.104.0/24"]
+  elasticache_subnets = ["10.0.105.0/24", "10.0.106.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
 
   # VPC Flow Logs
   flow_log_file_format = "plain-text"
+
+}
+
+resource "aws_elasticache_cluster" "app_cache" {
+  cluster_id      = "${var.environment}-redis"
+  engine          = "redis"
+  node_type       = "cache.t3.micro"
+  num_cache_nodes = 1
+  port            = 6379
 }
 
 resource "aws_db_instance" "app_database" {
-  instance_class = "db.t4g.micro"
+  instance_class    = "db.t4g.micro"
   allocated_storage = var.db_size
-  engine = "postgres"
+  engine            = "postgres"
 
   db_subnet_group_name = module.vpc.database_subnet_group_name
-  tags = {
+  tags                 = {
     Name = "${var.environment}-app-db"
   }
+}
+
+resource "aws_instance" "app_instance" {
+  instance_type = "t3.nano"
+  subnet_id     = module.vpc.private_subnets[0]
+  ami           = data.aws_ami.ubuntu
+  tags          = {
+    Name = "${var.environment}-app-instance"
+  }
+
 }
